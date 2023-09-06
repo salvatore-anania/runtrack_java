63b8fc9887b44ad01e6f35ee8b09bd3d680b1c72
salvatore-anania <salvatore.anania@laplateforme.io>
2023-09-06 11:16:55 +0200

    job01 implementation

---
+++ jour1/job01.java
@@ -0,0 +1,10 @@
+package jour1;
+
+public class job01 {
+    public static void main(String[] args) {
+        
+        System.out.println("Veuillez saisir votre prenom :");
+        String prenom = System.console().readLine();
+        System.out.println("Bonjour, " + prenom + " !");
+    }
+}
