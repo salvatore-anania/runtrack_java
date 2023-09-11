d96ceea9a39e4c36285cdc92e1411b03096c3ca9
salvatore-anania <salvatore.anania@laplateforme.io>
2023-09-11 15:50:19 +0200

    jour3 job05

---
+++ jour3/StreamsOperationExercice.java
@@ -0,0 +1,23 @@
+package jour3;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+
+public class StreamsOperationExercice {
+    
+    public static void main(String[] args){
+        // create a stream of integers from the args array
+
+        IntStream stream = Arrays.stream(args).mapToInt(Integer::parseInt).map(x -> x * 2).filter(x -> x < 10);
+        
+        // then create a list of the elements that are left
+        List<Integer> list = stream.boxed().toList();
+        System.out.println(list);
+
+
+        
+
+    }
+}
