21f0109bccb4ae5f69aa9d69d3bce70985d98690
salvatore-anania <salvatore.anania@laplateforme.io>
2023-09-13 10:55:24 +0200

    modification job05

--- jour5/StreamFilterExercice.java
+++ jour5/StreamFilterExercice.java
@@ -1,20 +1,21 @@
 package jour5;
 
+import java.util.stream.Stream;
+
 public class StreamFilterExercice {
     
     public static void main(String[] args){
-        // Use Stream API to filter all nunmbers given in arguments greater than an set value.
-
+        // Use the API Stream to filter all nunmbers given in arguments greater than an set value.
         // Example input:25 12 35 46 18 50 11 20 50
+        // Example output: 35 46 50 50
 
         int value = 25;
-        System.out.println("Numbers greater than " + value + ":");
-        for (String arg : args) {
-            int num = Integer.parseInt(arg);
-            if (num > value) {
-                System.out.println(num);
-            }
-        }
+        System.out.println("Numbers greater than " + value + " are:");
+        Stream.of(args)
+            .mapToInt(Integer::parseInt)
+            .filter(num -> num > value)
+            .forEach(System.out::println);
+        
 
     }
 }
---
+++ jour5/StreamMapExercice.java
@@ -0,0 +1,9 @@
+package jour5;
+
+public class StreamMapExercice {
+    
+    public void main(String[] args){
+        // Use Stream API pour convertir tous les noms (arguments) en majuscules.
+
+    }
+}
