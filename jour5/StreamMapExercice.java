8c7bdb4ac456cdcd97dd0dc40af748e2a7783aa2
salvatore-anania <salvatore.anania@laplateforme.io>
2023-09-13 10:57:35 +0200

    jour5 job06

--- jour5/StreamMapExercice.java
+++ jour5/StreamMapExercice.java
@@ -1,9 +1,16 @@
 package jour5;
 
+import java.util.stream.Stream;
+
 public class StreamMapExercice {
     
-    public void main(String[] args){
+    public static void main(String[] args){
         // Use Stream API pour convertir tous les noms (arguments) en majuscules.
+        // Exemple d'entrée: jean paul pierre
+        // Exemple de sortie: JEAN PAUL PIERRE
 
+        Stream.of(args)
+            .map(String::toUpperCase)
+            .forEach(System.out::println);
     }
 }
